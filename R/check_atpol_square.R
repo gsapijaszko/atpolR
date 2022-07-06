aeb0a9cbdba45e6245af43cba7e065a204a0ec9d
sapi <grzegorz@sapijaszko.net>
2022-07-06 22:38:10 +0200

    adding vignette

--- R/check_atpol_square.R
+++ R/check_atpol_square.R
@@ -1,6 +1,6 @@
 #' Reverse engineering of published ATPOL grids
 #'
-#' checkATPOLSquare() do a reverse enginnering of published ATPOL grids species,
+#' check_atpol_square() do a reverse enginnering of published ATPOL grids species,
 #' especially those published in \insertCite{zajacAtlasRozmieszczeniaRoslin2001;textual}{atpolR}
 #' @importFrom sf st_buffer
 #' @importFrom terra crop vect
@@ -11,12 +11,12 @@
 #' @param distance \code{st_buffer} distance from centroid point for which the check is done, default 1200 m
 #' @returns "YES" or "?" for given SF point
 #'
-#' @usage checkATPOLSquare(centroid, raster, distance)
+#' @usage check_atpol_square(centroid, raster, distance)
 #' @export
 #' @references
 #'     \insertAllCited{}
 #'
-checkATPOLSquare <- function(centroid, raster, distance = 1200) {
+check_atpol_square <- function(centroid, raster, distance = 1200) {
   if (!grepl("ID\\[\"EPSG\",2180\\]\\]",terra::crs({{raster}}))) {
     print("Raster has to be in EPSG:2180 projection")
     print("Try terra::project(raster, \"EPSG:2180\")")
