f3812d127ef35b887c31a5fffe5adae7792e0e80
gsapijaszko <grzegorz@sapijaszko.net>
2022-07-06 15:53:43 +0200

    documentation update

--- R/check_atpol_square.R
+++ R/check_atpol_square.R
@@ -1,19 +1,20 @@
 #' Reverse engineering of published ATPOL grids
 #'
 #' checkATPOLSquare() do a reverse enginnering of published ATPOL grids species,
-#' especially those published in: Zając, Adam, and Maria Zając, eds. 2001.
-#' Atlas rozmieszczenia roślin naczyniowych w Polsce. Distribution Atlas of Vascular Plants in Poland.
-#' Kraków: Laboratory of Computer Chorology - Institute of Botany - Jagiellonian University.
+#' especially those published in \insertCite{zajacAtlasRozmieszczeniaRoslin2001;textual}{atpolR}
 #' @importFrom sf st_buffer
 #' @importFrom terra crop vect
-#' @param centroid SF point geometry for which the check is performed,
+#' @importFrom Rdpack reprompt
+#' @param centroid Simple Feature point geometry for which the check is performed,
 #' usually it corresponds to centroid of ATPOL 10km x 10km grid
 #' @param raster geocoded raster, it has to be in EPSG:2180 projection
-#' @param distance st_buffer distance from centroid point for which the check is done, default 1200 m
+#' @param distance \code{st_buffer} distance from centroid point for which the check is done, default 1200 m
 #' @returns "YES" or "?" for given SF point
 #'
 #' @usage checkATPOLSquare(centroid, raster, distance)
 #' @export
+#' @references
+#'     \insertAllCited{}
 #'
 checkATPOLSquare <- function(centroid, raster, distance = 1200) {
   if (!grepl("ID\\[\"EPSG\",2180\\]\\]",terra::crs({{raster}}))) {
--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,9 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   packageStartupMessage("This is an atpolR package")
-
-
   invisible()
+}
+
+.onLoad <- function(lib, pkg){
+  Rdpack::Rdpack_bibstyles(package = pkg, authors = "LongNames")
+  invisible(NULL)
 }
